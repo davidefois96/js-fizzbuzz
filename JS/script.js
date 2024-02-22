4125d25c993eb9f365f577dbcc9fa60f4b6c6ccf
davidefois96 <davidefois360@gmail.com>
2024-02-22 16:07:24 +0100

    [add] new code

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,37 @@
 
 
-for(let i=1; i<=100; i++){
+const Container = document.getElementById('Container');
 
+
+for(let i = 1; i <= 100; i++){
   let content = i ;
+  const box = document.createElement('div');
+  box.className = 'box';
+ 
+  Container.append(box);
 
 
-  if(i%3===0){
+  if(i%3===0) {
 
-    content='Fizz';
+   content='Fizz';
+   box.classList.add('bg-danger')
 
-    
-    
   }if(i%5===0){
 
     content='Buzz';
+    box.classList.add('bg-info')
 
     
 
   }if(i%3===0 && i%5===0){
 
-    content='FizzBuzz'
+    content='FizzBuzz';
+    box.classList.add('bg-warning')
 
 
   }
 
-   
-  console.log(content);
-  
-
+  box.innerHTML = content;
 
 
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ JS-FIZZBUZZ
 
 ## Descrizione procedimento per la risoluzione dell' esercizio
 
-1. Creo programma che stampi in console i numeri da 1 a 100
+1. Creo programma che stampi grazie ad un ciclo in console i numeri da 1 a 100
 
 2. Per i multipli di 3 stampo Fizz al posto del numero
 
@@ -11,3 +11,4 @@ JS-FIZZBUZZ
 
 4. Per i multipli di 3 e 5 stampo FizzBuzz
 
+5. Creo un container nel DOM , aggiungendo attraverso la funzione `append()` i miei box e dandoli un colore differente a seconda delle casistiche sopra citate
